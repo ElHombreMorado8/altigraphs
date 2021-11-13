3c9ee799f631ea827f04fcc09481022eb2972c37
Iris Murillo <irismurillorobla@gmail.com>
2021-11-13 20:04:44 +0100

    final changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const fileinclude = require('gulp-file-include');
 const gulp = require('gulp');
 
 gulp.task('fileinclude', function() {
-  return gulp.src(['src/inicio.html', 'src/crear_puerto1.html', 'src/crear_puerto2.html', 'src/login.html', 'src/profile.html', 'src/edit_profile.html', 'src/mountain_port.html'])
+  return gulp.src(['src/inicio.html', 'src/crear_puerto1.html', 'src/crear_puerto2.html', 'src/login.html', 'src/profile.html', 'src/edit_profile.html', 'src/mountain_port.html', 'src/search_results.html'])
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@file'
